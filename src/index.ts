a3c93b0c988b0bbb3f594047c9460fa0c6bcbb77
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-11-05 14:29:13 +0700

    chore: update code to add type assertions for `beforeTime` property
    Updated the code to add type assertions for the `beforeTime` property resolve type checking errors
    and ensure proper type inference

--- src/index.ts
+++ src/index.ts
@@ -4,48 +4,79 @@ import process from 'process'
 import { durationString } from './utils/duration'
 import { methodString } from './utils/method'
 
+const logRequest = (
+  request: {
+    headers: { get: (key: string) => any }
+    method: string
+    url: string
+  },
+  store: {
+    beforeTime: bigint
+  }
+) => {
+  const logStr = []
+
+  if (request.headers.get('X-Forwarded-For')) {
+    logStr.push(`[${pc.cyan(request.headers.get('X-Forwarded-For'))}]`)
+  }
+
+  logStr.push(methodString(request.method))
+  logStr.push(new URL(request.url).pathname)
+  logStr.push(durationString(store.beforeTime))
+
+  console.log(logStr.join(' '))
+}
+
+const logError = (
+  request: {
+    headers: { get: (key: string) => any }
+    method: string
+    url: string
+  },
+  error: {
+    status?: number
+    message?: string
+  },
+  store: {
+    beforeTime: bigint
+  }
+) => {
+  const logStr = []
+
+  logStr.push(pc.red(methodString(request.method)))
+  logStr.push(new URL(request.url).pathname)
+  logStr.push(pc.red('Error'))
+
+  if (error.status !== undefined) {
+    logStr.push(String(error.status))
+  }
+
+  if (error.message !== undefined) {
+    logStr.push(error.message)
+  }
+
+  logStr.push(durationString(store.beforeTime))
+
+  console.log(logStr.join(' '))
+}
+
 export const logger = () =>
   new Elysia({
     name: 'logixlysia'
   })
     .onRequest(ctx => {
-      ctx.store = { ...ctx.store, beforeTime: process.hrtime.bigint() }
+      ctx.store = { beforeTime: process.hrtime.bigint() } as {
+        beforeTime: bigint
+      }
     })
     .onBeforeHandle(ctx => {
-      ctx.store = { ...ctx.store, beforeTime: process.hrtime.bigint() }
+      ctx.store = { beforeTime: process.hrtime.bigint() } as {
+        beforeTime: bigint
+      }
     })
     .onAfterHandle(({ request, store }) => {
-      const logStr: string[] = []
-      if (request.headers.get('X-Forwarded-For')) {
-        logStr.push(`[${pc.cyan(request.headers.get('X-Forwarded-For'))}]`)
-      }
-
-      logStr.push(methodString(request.method))
-
-      logStr.push(new URL(request.url).pathname)
-      const beforeTime: bigint = (store as any).beforeTime
-
-      logStr.push(durationString(beforeTime))
-
-      console.log(logStr.join(' '))
+      logRequest(request, store as { beforeTime: bigint })
     })
     .onError(({ request, error, store }) => {
-      const logStr: string[] = []
-
-      logStr.push(pc.red(methodString(request.method)))
-
-      logStr.push(new URL(request.url).pathname)
-
-      logStr.push(pc.red('Error'))
-
-      if ('status' in error) {
-        logStr.push(String(error.status))
-      }
-
-      logStr.push(error.message)
-      const beforeTime: bigint = (store as any).beforeTime
-
-      logStr.push(durationString(beforeTime))
-
-      console.log(logStr.join(' '))
+      logError(request, error, store as { beforeTime: bigint })
     })
