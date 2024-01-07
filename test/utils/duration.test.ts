dabdbbff04c3e4890d5851fab02b898aaaf9593a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-07 10:21:24 +0700

    test(duration): use test cases instead

--- test/utils/duration.test.ts
+++ test/utils/duration.test.ts
@@ -2,33 +2,19 @@ import { describe, expect, it } from 'bun:test'
 import durationString from '~/utils/duration'
 
 describe('Duration String', () => {
-  it('Returns a formatted string for seconds', () => {
-    const beforeTime = process.hrtime.bigint() - BigInt(1e9) // 1 second
-    const result = durationString(beforeTime)
-    expect(result).toMatch('s')
-  })
+  it('Returns a duration string with different units', () => {
+    const testCases = [
+      [1e9, 's'],
+      [1e6, 'ms'],
+      [1e3, 'µs'],
+      [1, 'ns']
+    ]
 
-  it('Returns a formatted string for milliseconds', () => {
-    const beforeTime = process.hrtime.bigint() - BigInt(1e6) // 1 millisecond
-    const result = durationString(beforeTime)
-    expect(result).toMatch('ms')
-  })
+    for (const [nanoseconds, unit] of testCases) {
+      const beforeTime = process.hrtime.bigint() - BigInt(String(nanoseconds))
 
-  it('Returns a formatted string for microseconds', () => {
-    const beforeTime = process.hrtime.bigint() - BigInt(1e3) // 1 microsecond
-    const result = durationString(beforeTime)
-    expect(result).toMatch('µs')
-  })
-
-  it('Returns a formatted string for nanoseconds', () => {
-    const beforeTime = process.hrtime.bigint() - BigInt(1) // 1 nanosecond
-    const result = durationString(beforeTime)
-    expect(result).toMatch('ns')
-  })
-
-  it('Returns an empty string for negative time difference', () => {
-    const beforeTime = process.hrtime.bigint() + BigInt(1e6) // Negative duration
-    const result = durationString(beforeTime)
-    expect(result).toMatch('')
+      const result = durationString(beforeTime)
+      expect(result).toMatch(String(unit))
+    }
   })
 })
