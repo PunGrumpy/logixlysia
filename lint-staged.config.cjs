6152071ef1ab6c7d72e8c7817baf53670aa25d04
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-07 10:23:52 +0700

    chore(pre-commit): add test before commit

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -9,11 +9,17 @@ const isPnpm = existsSync(join(process.cwd(), 'pnpm-lock.yaml'))
 const packageManager = isBun ? 'bun' : isYarn ? 'yarn' : isPnpm ? 'pnpm' : 'npm'
 
 const options = {
+  // TypeScript & JavaScript files
   '**/*.(ts|tsx)': () => `${packageManager} tsc --noEmit`,
   '**/*.(ts|tsx|js)': filenames => [
     `${packageManager} eslint --fix ${filenames.join(' ')}`,
     `${packageManager} prettier --write ${filenames.join(' ')}`
   ],
+  '**/*.(css|less|scss)': filenames =>
+    `${packageManager} test --timeout 5000 --coverage --update-snapshots ${filenames.join(
+      ' '
+    )}`,
+  // Markdown & JSON files
   '**/*.(md|json)': filenames =>
     `${packageManager} prettier --write ${filenames.join(' ')}`
 }
