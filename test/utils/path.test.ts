2a84cee51db5a100f9518bf58b6c9eb299759d6d
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-26 00:33:11 +0700

    test(path): make coverage testing

--- test/utils/path.test.ts
+++ test/utils/path.test.ts
@@ -1,10 +1,35 @@
 import { describe, expect, it } from 'bun:test'
+import { RequestInfo } from '~/types/RequestInfo'
 import pathString from '~/utils/path'
 
 describe('Path String', () => {
-  it('Returns the path string from a URL', () => {
-    const testPath: any = { url: 'https://www.example.com/path/to/resource' }
+  it('Extracts the pathname from a valid URL', () => {
+    const testPath: RequestInfo = {
+      url: 'https://www.example.com/path/to/resource',
+      headers: new Map(),
+      method: 'GET'
+    }
     const result = pathString(testPath)
-    expect(result).toMatch('/path/to/resource')
+    expect(result).toBe('/path/to/resource')
+  })
+
+  it('Handles malformed URL gracefully', () => {
+    const testPath: RequestInfo = {
+      url: 'invalid url',
+      headers: new Map(),
+      method: 'GET'
+    }
+    const result = pathString(testPath)
+    expect(result).toBeUndefined()
+  })
+
+  it('Returns undefined if the URL is missing', () => {
+    const testPath: RequestInfo = {
+      url: '',
+      headers: new Map(),
+      method: 'GET'
+    }
+    const result = pathString(testPath)
+    expect(result).toBeUndefined()
   })
 })
