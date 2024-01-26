7ba1e2bd5eea44ddd152c3d0f1fd021ed87810e4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-01-26 14:01:53 +0700

    test(duration): add description to test cases for separate name

--- test/utils/duration.test.ts
+++ test/utils/duration.test.ts
@@ -2,19 +2,35 @@ import { describe, expect, it } from 'bun:test'
 import durationString from '~/utils/duration'
 
 describe('Duration String', () => {
-  it('Generates a string representing the duration in appropriate units', () => {
-    const testCases = [
-      [1e9, 's'],
-      [1e6, 'ms'],
-      [1e3, 'µs'],
-      [1, 'ns']
+  const testCases = [
+    [
+      'Generates a string representing the duration in Seconds (s) unit',
+      1e9,
+      's'
+    ],
+    [
+      'Generates a string representing the duration in Milliseconds (ms) unit',
+      1e6,
+      'ms'
+    ],
+    [
+      'Generates a string representing the duration in Microseconds (µs) unit',
+      1e3,
+      'µs'
+    ],
+    [
+      'Generates a string representing the duration in Nanoseconds (ns) unit',
+      1,
+      'ns'
     ]
+  ]
 
-    for (const [nanoseconds, unit] of testCases) {
+  for (const [description, nanoseconds, unit] of testCases) {
+    it(`${description}`, () => {
       const beforeTime = process.hrtime.bigint() - BigInt(String(nanoseconds))
-
       const result = durationString(beforeTime)
+
       expect(result).toMatch(String(unit))
-    }
-  })
+    })
+  }
 })
