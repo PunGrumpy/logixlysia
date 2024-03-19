e55c2275b0cc3dc5671e4cb870a4cc535a2d7682
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-03-19 08:26:57 +0700

    test(index): add unit tests for `logixlysia` server
    Add unit tests to ensure the Logixlysia server responds correctly to GET and POST requests.

---
+++ test/logixlysia.test.ts
@@ -0,0 +1,51 @@
+import { Elysia } from 'elysia'
+import { edenTreaty } from '@elysiajs/eden'
+import { describe, it, expect, beforeAll, beforeEach } from 'bun:test'
+import { logger } from '../src'
+
+describe('Logixlysia', () => {
+  let server: Elysia
+  let app: any
+  let logs: string[] = []
+
+  beforeAll(() => {
+    server = new Elysia()
+      .use(logger({ ip: false }))
+      .get('/', () => '🦊 Logixlysia Getting')
+      .post('logixlysia', () => '🦊 Logixlysia Posting')
+      .listen(3000)
+
+    app = edenTreaty<typeof server>('http://127.0.0.1:3000')
+  })
+
+  beforeEach(() => {
+    logs = []
+  })
+
+  it("Responds correctly to GET '/' requests", async () => {
+    const requestCount = 5
+
+    for (let i = 0; i < requestCount; i++) {
+      logs.push((await app.get('/')).data)
+    }
+
+    logs.forEach(log => {
+      expect(log).toBe('🦊 Logixlysia Getting')
+    })
+  })
+
+  it("Responds correctly to POST '/logixlysia' requests", async () => {
+    const requestCount = 5
+
+    for (let i = 0; i < requestCount; i++) {
+      const postResponse = await app.logixlysia.post({})
+      logs.push(
+        postResponse.status === 200 ? postResponse.data : postResponse.error
+      )
+    }
+
+    logs.forEach(log => {
+      expect(log).toBe('🦊 Logixlysia Posting')
+    })
+  })
+})
