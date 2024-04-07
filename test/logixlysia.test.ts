c334485e905c43dbf72a0abe33e5178cdbfa610b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-04-07 21:08:02 +0700

    test(index): separate to 2 testing

--- test/logixlysia.test.ts
+++ test/logixlysia.test.ts
@@ -3,93 +3,106 @@ import { edenTreaty } from '@elysiajs/eden'
 import { describe, it, expect, beforeAll, beforeEach } from 'bun:test'
 import { logger } from '../src'
 
-describe('Logixlysia', () => {
+describe('Logixlysia with IP logging enabled', () => {
   let server: Elysia
   let app: any
   let logs: string[] = []
 
-  describe('IP logging disabled', () => {
-    beforeAll(() => {
-      server = new Elysia()
-        .use(logger({ ip: false }))
-        .get('/', () => '🦊 Logixlysia Getting')
-        .post('logixlysia', () => '🦊 Logixlysia Posting')
-        .listen(3000)
-
-      app = edenTreaty<typeof server>('http://127.0.0.1:3000')
-    })
-
-    beforeEach(() => {
-      logs = []
-    })
-
-    it("Responds correctly to GET '/' requests", async () => {
-      const requestCount = 5
-
-      for (let i = 0; i < requestCount; i++) {
-        logs.push((await app.get('/')).data)
-      }
-
-      logs.forEach(log => {
-        expect(log).toBe('🦊 Logixlysia Getting')
+  beforeAll(() => {
+    server = new Elysia()
+      .use(logger({ ip: true }))
+      .get('/', ctx => {
+        const ipAddress = ctx.request.headers.get('x-forwarded-for') || 'null'
+        return '🦊 Logixlysia Getting'
       })
-    })
+      .post('logixlysia', () => '🦊 Logixlysia Posting')
+      .listen(3000)
 
-    it("Responds correctly to POST '/logixlysia' requests", async () => {
-      const requestCount = 5
+    app = edenTreaty<typeof server>('http://127.0.0.1:3000')
+  })
 
-      for (let i = 0; i < requestCount; i++) {
-        const postResponse = await app.logixlysia.post({})
-        logs.push(
-          postResponse.status === 200 ? postResponse.data : postResponse.error
-        )
-      }
+  beforeEach(() => {
+    logs = []
+  })
 
-      logs.forEach(log => {
-        expect(log).toBe('🦊 Logixlysia Posting')
+  it("Logs incoming IP address for GET '/' requests when X-Forwarded-For header is present", async () => {
+    const requestCount = 5
+
+    for (let i = 0; i < requestCount; i++) {
+      await app.get('/', {
+        headers: { 'X-Forwarded-For': '192.168.1.1' }
       })
-    })
+    }
 
-    it('Throws an error when attempting to post to an undefined route', async () => {
-      const response = await app.undefinedRoute.post({})
-      const error = response.error
-
-      expect(response.status).toBe(404)
-      expect(error).toBeInstanceOf(Error)
+    logs.forEach(log => {
+      expect(log).toMatch(/^IP: \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
     })
   })
 
-  describe('IP logging enabled', () => {
-    beforeAll(() => {
-      server = new Elysia()
-        .use(logger({ ip: true }))
-        .get('/', () => '🦊 Logixlysia Getting')
-        .post('logixlysia', () => '🦊 Logixlysia Posting')
-        .listen(3000)
+  it("Logs 'null' for GET '/' requests when X-Forwarded-For header is not present", async () => {
+    const requestCount = 5
 
-      app = edenTreaty<typeof server>('http://127.0.0.1:3000')
+    for (let i = 0; i < requestCount; i++) {
+      const response = await app.get('/')
+    }
+
+    logs.forEach(log => {
+      expect(log).toBe('IP: null')
     })
+  })
+})
 
-    beforeEach(() => {
-      logs = []
+describe('Logixlysia with IP logging disabled', () => {
+  let server: Elysia
+  let app: any
+  let logs: string[] = []
+
+  beforeAll(() => {
+    server = new Elysia()
+      .use(logger({ ip: false }))
+      .get('/', () => '🦊 Logixlysia Getting')
+      .post('logixlysia', () => '🦊 Logixlysia Posting')
+      .listen(3000)
+
+    app = edenTreaty<typeof server>('http://127.0.0.1:3000')
+  })
+
+  beforeEach(() => {
+    logs = []
+  })
+
+  it("Responds correctly to GET '/' requests", async () => {
+    const requestCount = 5
+
+    for (let i = 0; i < requestCount; i++) {
+      logs.push((await app.get('/')).data)
+    }
+
+    logs.forEach(log => {
+      expect(log).toBe('🦊 Logixlysia Getting')
     })
+  })
 
-    it("Logs incoming IP address for GET '/' requests when X-Forwarded-For header is present", async () => {
-      logs.forEach(log => {
-        expect(log).toMatch(/^IP: \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
-      })
+  it("Responds correctly to POST '/logixlysia' requests", async () => {
+    const requestCount = 5
+
+    for (let i = 0; i < requestCount; i++) {
+      const postResponse = await app.logixlysia.post({})
+      logs.push(
+        postResponse.status === 200 ? postResponse.data : postResponse.error
+      )
+    }
+
+    logs.forEach(log => {
+      expect(log).toBe('🦊 Logixlysia Posting')
     })
+  })
 
-    it("Logs incoming IP address for GET '/' requests when X-Forwarded-For header is not present", async () => {
-      const requestCount = 5
+  it('Throws an error when attempting to post to an undefined route', async () => {
+    const response = await app.undefinedRoute.post({})
+    const error = response.error
 
-      for (let i = 0; i < requestCount; i++) {
-        const response = await app.get('/')
-      }
-
-      logs.forEach(log => {
-        expect(log).toMatch(/^IP: \d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/)
-      })
-    })
+    expect(response.status).toBe(404)
+    expect(error).toBeInstanceOf(Error)
   })
 })
