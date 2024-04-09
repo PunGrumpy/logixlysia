af7d9ec5b2bb5ff5942d1a586be23a9d23a2c1cf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-04-09 16:24:12 +0700

    feat(logger): add log filtering functional
    Implemented log filtering to allow users to specify criteria for filtering which logs are captured
    and processed
    
    close #38

--- src/logger.ts
+++ src/logger.ts
@@ -15,6 +15,55 @@ import {
 } from './types'
 
 /**
+ * Filters log messages.
+ *
+ * @param {LogLevel} logLevel The log level.
+ * @param {number} status The status code.
+ * @param {string} method The method.
+ * @param {Options} options The options.
+ * @returns {boolean} `true` if the log message should be logged, otherwise `false`.
+ */
+function filterLog(
+  logLevel: LogLevel,
+  status: number,
+  method: string,
+  options?: Options
+): boolean {
+  const filter = options?.config?.logFilter
+
+  if (!filter) return true
+
+  // Level
+  if (filter.level) {
+    if (Array.isArray(filter.level)) {
+      if (!filter.level.includes(logLevel)) return false
+    }
+  } else {
+    if (filter.level !== logLevel) return false
+  }
+
+  // Status
+  if (filter.status) {
+    if (Array.isArray(filter.status)) {
+      if (!filter.status.includes(status)) return false
+    }
+  } else {
+    if (filter.status !== status) return false
+  }
+
+  // Method
+  if (filter.method) {
+    if (Array.isArray(filter.method)) {
+      if (!filter.method.includes(method)) return false
+    }
+  } else {
+    if (filter.method !== method) return false
+  }
+
+  return true
+}
+
+/**
  * Logs a message.
  *
  * @param {LogLevel} level The log level.
@@ -30,6 +79,10 @@ function log(
   store: StoreData,
   options?: Options
 ): void {
+  if (!filterLog(level, data.status || 200, request.method, options)) {
+    return
+  }
+
   const logMessage = buildLogMessage(level, request, data, store, options)
   console.log(logMessage)
 }
