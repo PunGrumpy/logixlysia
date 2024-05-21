61530a8ece0a5ce83df1448984eb057e7c95e998
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-05-21 15:22:24 +0700

    chore(eslint): add `simple-import-sort` configuration

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,6 +1,6 @@
-const eslintConfig = [
+module.exports = [
   {
-    files: ['*.ts', '*.tsx'],
+    files: ['*.ts'],
     languageOptions: {
       parser: '@typescript-eslint/parser',
       parserOptions: {
@@ -9,7 +9,8 @@ const eslintConfig = [
       }
     },
     plugins: {
-      '@typescript-eslint': require('@typescript-eslint/eslint-plugin')
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      'simple-import-sort': require('eslint-plugin-simple-import-sort')
     },
     rules: {
       '@typescript-eslint/ban-types': 'off',
@@ -21,21 +22,28 @@ const eslintConfig = [
       '@typescript-eslint/no-var-requires': 'error',
       'no-mixed-spaces-and-tabs': 'off',
       'no-case-declarations': 'off',
-      'no-extra-semi': 'off'
+      'no-extra-semi': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
     }
   },
   {
     files: ['*.js', '*.cjs', '*.mjs'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+      }
+    },
+    plugins: {
+      'simple-import-sort': require('eslint-plugin-simple-import-sort')
     },
     rules: {
       'no-mixed-spaces-and-tabs': 'off',
       'no-case-declarations': 'off',
-      'no-extra-semi': 'off'
+      'no-extra-semi': 'off',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error'
     }
   }
 ]
-
-module.exports = eslintConfig
