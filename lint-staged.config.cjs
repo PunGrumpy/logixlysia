7023852b14dc5aa38cea495071d0469044dffc05
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-05-21 16:20:46 +0700

    chore(lint-staged): remove useless coding

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,22 +1,8 @@
-const { existsSync } = require('fs')
-const { join } = require('path')
-
-const isBun = existsSync(join(process.cwd(), 'bun.lockb'))
-const isYarn = existsSync(join(process.cwd(), 'yarn.lock'))
-const isPnpm = existsSync(join(process.cwd(), 'pnpm-lock.yaml'))
-
-const packageManager = isBun ? 'bun' : isYarn ? 'yarn' : isPnpm ? 'pnpm' : 'npm'
-
-const options = {
-  // TypeScript & JavaScript files
-  '**/*.(ts)': () => `${packageManager} tsc --noEmit`,
+module.exports = {
+  '**/*.(ts)': () => `bun tsc --noEmit`,
   '**/*.(ts|js|cjs)': filenames => [
-    `${packageManager} eslint --fix ${filenames.join(' ')}`,
-    `${packageManager} prettier --write ${filenames.join(' ')}`
+    `bun eslint --fix ${filenames.join(' ')}`,
+    `bun prettier --write ${filenames.join(' ')}`
   ],
-  // Markdown & JSON files
-  '**/*.(md|json)': filenames =>
-    `${packageManager} prettier --write ${filenames.join(' ')}`
+  '**/*.(md|json)': filenames => `bun prettier --write ${filenames.join(' ')}`
 }
-
-module.exports = options
