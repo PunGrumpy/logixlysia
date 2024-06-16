f22908bd2be97d7eaf0384bc11ee8279a548e8f6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-06-16 22:31:49 +0700

    style(formatting): make upper-case

--- test/logger/filter.test.ts
+++ test/logger/filter.test.ts
@@ -3,57 +3,57 @@ import { describe, expect, it } from 'bun:test'
 import { filterLog } from '~/logger/filter'
 import { LogLevel, Options } from '~/types'
 
-describe('filterLog', () => {
+describe('Filter log', () => {
   const logLevel: LogLevel = 'info'
   const status = 200
   const method = 'GET'
 
-  it('should return true if no filter is provided', () => {
+  it('Should return true if no filter is provided', () => {
     expect(filterLog(logLevel, status, method)).toBe(true)
   })
 
-  it('should filter by log level (single value)', () => {
+  it('Should filter by log level (single value)', () => {
     const options: Options = { config: { logFilter: { level: 'error' } } }
     expect(filterLog(logLevel, status, method, options)).toBe(false)
   })
 
-  it('should filter by log level (array)', () => {
+  it('Should filter by log level (array)', () => {
     const options: Options = {
       config: { logFilter: { level: ['error', 'info'] } }
     }
     expect(filterLog(logLevel, status, method, options)).toBe(true)
   })
 
-  it('should filter by status (single value)', () => {
+  it('Should filter by status (single value)', () => {
     const options: Options = { config: { logFilter: { status: 404 } } }
     expect(filterLog(logLevel, status, method, options)).toBe(false)
   })
 
-  it('should filter by status (array)', () => {
+  it('Should filter by status (array)', () => {
     const options: Options = { config: { logFilter: { status: [200, 404] } } }
     expect(filterLog(logLevel, status, method, options)).toBe(true)
   })
 
-  it('should filter by method (single value)', () => {
+  it('Should filter by method (single value)', () => {
     const options: Options = { config: { logFilter: { method: 'POST' } } }
     expect(filterLog(logLevel, status, method, options)).toBe(false)
   })
 
-  it('should filter by method (array)', () => {
+  it('Should filter by method (array)', () => {
     const options: Options = {
       config: { logFilter: { method: ['GET', 'POST'] } }
     }
     expect(filterLog(logLevel, status, method, options)).toBe(true)
   })
 
-  it('should return false if any filter condition is not met', () => {
+  it('Should return false if any filter condition is not met', () => {
     const options: Options = {
       config: { logFilter: { level: 'info', status: 404, method: 'POST' } }
     }
     expect(filterLog(logLevel, status, method, options)).toBe(false)
   })
 
-  it('should return true if all filter conditions are met', () => {
+  it('Should return true if all filter conditions are met', () => {
     const options: Options = {
       config: { logFilter: { level: 'info', status: 200, method: 'GET' } }
     }
