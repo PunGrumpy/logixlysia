f82175bec3d37fccd5b6c3a8c6b19aad387c8144
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-07-11 18:04:05 +0700

    chore(script): change `prettier` to `format`

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.(ts)': () => `bun tsc --noEmit`,
   '**/*.(ts|js|cjs)': filenames => [
     `bun eslint --fix ${filenames.join(' ')}`,
-    `bun prettier --write ${filenames.join(' ')}`
+    `bun format --write ${filenames.join(' ')}`
   ],
-  '**/*.(md|json)': filenames => `bun prettier --write ${filenames.join(' ')}`
+  '**/*.(md|json)': filenames => `bun format --write ${filenames.join(' ')}`
 }
