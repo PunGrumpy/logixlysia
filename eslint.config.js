64035596912b9d2612c6fd662471705514c0ffd4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-08 17:32:53 +0700

    chore(eslint): use eslint plugin correct way

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
 import pluginJs from '@eslint/js'
-import eslintPlugin from '@typescript-eslint/eslint-plugin'
-import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort'
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   {
     languageOptions: {
       globals: globals.browser
     },
     plugins: {
-      '@typescript-eslint': eslintPlugin,
-      'simple-import-sort': eslintPluginSimpleImportSort
+      'simple-import-sort': simpleImportSort
     },
     rules: {
       'no-undef': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
       'simple-import-sort/imports': 'error',
       'simple-import-sort/exports': 'error'
     }
