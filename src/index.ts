7c0c0b995775ea058853b872d7860b4f789cdb45
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-05-16 16:35:22 +0700

    feat(logging): add custom log methods and context support
    - Introduce new logging methods: `info`, `error`, `warn`, and `debug` in the Logger interface.
    - Enhance log context handling to include additional metadata in log messages.
    - Update `logixlysia` function to enable custom logging behavior when a custom logger is present.
    - Extend `StoreData` interface to track custom logging state.
    
    closes #94

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { Elysia } from 'elysia'
 
 import { startServer } from './extensions'
 import { getStatusCode } from './helpers/status'
-import type { HttpError, Options, Server } from './interfaces'
+import type { HttpError, Options, Server, StoreData } from './interfaces'
 import { createLogger } from './logger'
 
 export default function logixlysia(options?: Options): Elysia {
@@ -18,9 +18,19 @@ export default function logixlysia(options?: Options): Elysia {
       }
     })
     .onRequest(ctx => {
-      ctx.store = { beforeTime: process.hrtime.bigint() }
+      ctx.store = {
+        beforeTime: process.hrtime.bigint(),
+        logger: log,
+        hasCustomLog: true
+      }
     })
     .onAfterHandle({ as: 'global' }, ({ request, set, store }) => {
+      const storeData = store as StoreData
+
+      if (storeData.hasCustomLog) {
+        return
+      }
+
       const status = getStatusCode(set.status || 200)
       log.log(
         'INFO',
@@ -29,7 +39,7 @@ export default function logixlysia(options?: Options): Elysia {
           status,
           message: String(set.headers?.['x-message'] || '')
         },
-        store as { beforeTime: bigint }
+        storeData
       )
     })
     .onError({ as: 'global' }, ({ request, error, set, store }) => {
@@ -37,7 +47,7 @@ export default function logixlysia(options?: Options): Elysia {
       log.handleHttpError(
         request,
         { ...error, status } as HttpError,
-        store as { beforeTime: bigint }
+        store as StoreData
       )
     })
 }
@@ -52,5 +62,6 @@ export type {
   StoreData,
   HttpError,
   Transport,
-  Options
+  Options,
+  LogixlysiaContext
 } from './interfaces'
