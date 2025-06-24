199e6a80252813e3372f61128adc563bfea46930
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-06-24 23:33:40 +0700

    chore(deps): update @radix-ui/react-slot, @radix-ui/react-tabs, and sonner dependencies
    - Bumped @radix-ui/react-slot from 1.2.0 to 1.2.3
    - Bumped @radix-ui/react-tabs from 1.1.9 to 1.1.12
    - Bumped sonner from 2.0.3 to 2.0.5
    
    These updates improve compatibility and performance by incorporating the latest versions of these dependencies.

--- website/components/ui/sonner.tsx
+++ website/components/ui/sonner.tsx
@@ -1,22 +1,22 @@
-'use client'
+"use client"
 
-import { useTheme } from 'next-themes'
-import { Toaster as Sonner, type ToasterProps } from 'sonner'
+import { useTheme } from "next-themes"
+import { Toaster as Sonner, ToasterProps } from "sonner"
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = 'system' } = useTheme()
+  const { theme = "system" } = useTheme()
 
   return (
     <Sonner
+      theme={theme as ToasterProps["theme"]}
       className="toaster group"
       style={
         {
-          '--normal-bg': 'var(--popover)',
-          '--normal-text': 'var(--popover-foreground)',
-          '--normal-border': 'var(--border)'
+          "--normal-bg": "var(--popover)",
+          "--normal-text": "var(--popover-foreground)",
+          "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
-      theme={theme as ToasterProps['theme']}
       {...props}
     />
   )
