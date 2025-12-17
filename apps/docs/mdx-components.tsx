e65a4cca446356c66e0357423d11ad723e90ece6
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-17 20:36:45 +0700

    feat(docs): enhance MDX components with dynamic link support
    - Added a custom link component that conditionally renders either a DynamicLink or a standard anchor tag based on the href value, improving navigation for internal links.
    - This change enhances the documentation's interactivity and user experience by ensuring proper link handling.

--- apps/docs/mdx-components.tsx
+++ apps/docs/mdx-components.tsx
@@ -1,9 +1,18 @@
+import { DynamicLink } from 'fumadocs-core/dynamic-link'
 import defaultMdxComponents from 'fumadocs-ui/mdx'
 import type { MDXComponents } from 'mdx/types'
+import type { ComponentProps } from 'react'
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
-    ...components
+    ...components,
+
+    a: ({ href, ...props }: ComponentProps<typeof DynamicLink>) =>
+      href?.startsWith('/') ? (
+        <DynamicLink {...props} href={href} />
+      ) : (
+        <a {...props} href={href} />
+      )
   }
 }
