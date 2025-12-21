ccc3542820c3f744ff79136e287e04944507b909
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-21 21:09:58 +0700

    feat(elysia-node): add new Elysia Node example with logging and custom routes
    - Introduced a new example application `elysia-node-example` with a complete setup including `package.json` and `tsconfig.json`.
    - Implemented custom routers for logging and error handling, including `boomRouter`, `customRouter`, `pinoRouter`, and `statusRouter`.
    - Configured the application to use `@elysiajs/node` and `@elysiajs/swagger` for enhanced functionality and documentation.
    - Updated `bun.lock` to include new dependencies and ensure proper package management.

---
+++ apps/elysia-node/src/routers/index.ts
@@ -0,0 +1,25 @@
+import Elysia from 'elysia'
+import { logixlysia } from 'logixlysia'
+import { boomRouter } from './boom'
+import { customRouter } from './custom'
+import { pinoRouter } from './pino'
+import { statusRouter } from './status'
+
+export const routers = new Elysia()
+  .use(
+    logixlysia({
+      config: {
+        timestamp: {
+          translateTime: 'yyyy-mm-dd HH:MM:ss'
+        },
+        customLogFormat:
+          '🦊 {now} {level} {duration} {method} {pathname} {status} {message} {ip} {context}',
+        logFilePath: './logs/example.log',
+        ip: true
+      }
+    })
+  )
+  .use(customRouter)
+  .use(pinoRouter)
+  .use(statusRouter)
+  .use(boomRouter)
