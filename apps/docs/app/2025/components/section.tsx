c6f6537146df57c1f605c06645e0971d16d88ca4
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2025-12-25 21:35:43 +0700

    refactor(Section): update props type for improved flexibility
    - Changed the props type from a custom interface to HTMLProps for the Section component, allowing for greater flexibility in passing additional HTML attributes.
    - Maintained the existing functionality and structure of the component.

--- apps/docs/app/2025/components/section.tsx
+++ apps/docs/app/2025/components/section.tsx
@@ -1,13 +1,15 @@
-import type { ReactNode } from 'react'
+import type { HTMLProps } from 'react'
 import { cn } from '@/lib/utils'
 
-interface SectionProps {
-  children: ReactNode
-  className?: string
-}
-
-export const Section = ({ children, className }: SectionProps) => (
-  <section className={cn('container relative mx-auto px-4 md:px-8', className)}>
+export const Section = ({
+  children,
+  className,
+  ...props
+}: HTMLProps<HTMLDivElement>) => (
+  <section
+    className={cn('container relative mx-auto px-4 md:px-8', className)}
+    {...props}
+  >
     {children}
   </section>
 )
