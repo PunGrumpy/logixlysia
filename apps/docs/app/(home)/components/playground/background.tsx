0dbda80db12d0580e738e19068c9d81d0d62308a
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2026-01-08 22:11:34 +0700

    feat(playground): add production environment check for UnicornScene component
    - Introduce a production prop to the UnicornScene component to conditionally render based on the NODE_ENV variable.

--- apps/docs/app/(home)/components/playground/background.tsx
+++ apps/docs/app/(home)/components/playground/background.tsx
@@ -7,6 +7,7 @@ export const Background = () => (
   <div className="absolute inset-0">
     <UnicornScene
       height={1000}
+      production={process.env.NODE_ENV === 'production'}
       projectId={env.NEXT_PUBLIC_UNICORNSTUDIO_PROJECT_ID}
       width={1500}
     />
