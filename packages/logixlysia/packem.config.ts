ac742a0f3ce43bed612fe907bf29b33d0ccf78b7
pori <1960825664@qq.com>
2026-08-14 13:24:36 +0800

    refactor: 更新 package.json 和 packem 配置，调整模块导出和依赖项

--- packages/logixlysia/packem.config.ts
+++ packages/logixlysia/packem.config.ts
@@ -1,27 +1,13 @@
-// packem.config.ts
 import { defineConfig } from "@visulima/packem/config";
-import esbuild from "@visulima/packem/transformer/esbuild";
+import transformer from "@visulima/packem/transformer/esbuild";
 
 export default defineConfig({
-  declaration: true,
-
-  entries: [
-    { declaration: true, esm: true, input: "src/index.ts" },
-    { declaration: true, esm: true, input: "src/otel.ts" },
-    { declaration: true, esm: true, input: "src/ai.ts" },
-  ],
-
   externals: ["elysia", "chalk", "pino", "pino-pretty"],
   failOnWarn: false,
-  minify: true,
-
-  outDir: "dist",
-
   rollup: {
     patchTypes: false,
   },
-
   runtime: "node",
-  sourcemap: true,
-  transformer: esbuild,
+  sourcemap: false,
+  transformer,
 });
